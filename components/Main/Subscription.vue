8144e2efc1b70c49dd6eecca4ad82cb54f70907a
mariakhachatryann <mariakhachatryan23@gmail.com>
2023-10-07 02:07:47 +0400

    some changes in styles

--- components/Main/Subscription.vue
+++ components/Main/Subscription.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex justify-center w-full items-center lg:max-w-[960px] lg:min-w-[250px] sm:max-w-[250px] md:max-w-[250px]">
         <div class="flex my-16 h-auto justify-center items-center max-w-[1384px] bg-[#F1F7FE] rounded-3xl xl:mx-8 lg:flex-col-reverse lg:w-[960px] sm:max-w-[280px] md:mx-2">
-            <div class="max-w-[530px] mx-12 xl:mx-2 lg:max-w-[960px] lg:p-8 sm:p-2 sm:mx-0 md:mx-0">
+            <div class="max-w-[530px] mx-12 my-4 lg:max-w-full xl:mx-4">
                 <p class="text-[32px] xl:text-[28px] font-extrabold lg:text-[28px]">Subscription our newsletter and get updated every time</p>
                 <p class="font-base text-primaryGrey mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam morbi odio id dictum ut vestibulum. Tempor duis molestie proin nec. Interdum dolor dui venenatis elementum leo eleifend arcu, cursus justo.</p>
                 <div class="flex mt-8 sm:flex-col sm:gap-4">
