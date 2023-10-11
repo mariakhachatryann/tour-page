2153424ccbab238127419c873ca24a5f97f6c7bc
mariakhachatryann <mariakhachatryan23@gmail.com>
2023-10-11 17:55:55 +0400

    added particular tour page

---
+++ components/QntInput.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="flex gap-2">
+            <p class="text-[#19222C] mr-2">{{ props.label }}</p>
+            <p class="text-[#666C73] font-medium">(age {{ props.age }})</p>
+        </div>
+        <div class="flex mt-1 bg-primaryBlue rounded-lg w-[160px]">
+            <div @click="decr" class="flex justify-center items-center bg-primaryBlue rounded-lg py-3 w-14 text-white text-center cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="4" viewBox="0 0 16 4" fill="none">
+                    <path d="M16 0.474786V3.52685C16 3.75661 15.8139 3.94304 15.5838 3.94304H9.94226C9.71251 3.94304 6.0578 3.94304 6.0578 3.94304H0.416194C0.186254 3.94304 0 3.75661 0 3.52685V0.474786C0 0.244984 0.186254 0.0585938 0.416194 0.0585938H6.0578H9.94226H15.5838C15.8139 0.0585938 16 0.244984 16 0.474786Z" fill="white"/>
+                </svg>
+            </div>
+            <div class="py-3 w-16 bg-white text-center border-[1px] border-solid border-[#E6E6E7] rounded-lg">{{ props.qnt }}</div>
+            <div @click="incr" class="flex justify-center items-center bg-primaryBlue rounded-lg  py-3 w-14 text-white text-center cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                    <path d="M16 6.47396V9.52602C16 9.75578 15.8138 9.94221 15.5838 9.94221H9.94224V15.5837C9.94224 15.8138 9.75581 15.9999 9.52605 15.9999H6.47398C6.24436 15.9999 6.05779 15.8138 6.05779 15.5837V9.94221H0.416193C0.186253 9.94221 0 9.75578 0 9.52602V6.47396C0 6.24416 0.186253 6.05777 0.416193 6.05777H6.05779V0.416192C6.05779 0.186161 6.24432 0 6.47398 0H9.52605C9.75581 0 9.94224 0.186161 9.94224 0.416192V6.05777H15.5838C15.8139 6.05777 16 6.24416 16 6.47396Z" fill="white"/>
+                </svg>
+            </div>
+        </div>
+        <div class="max-w-[320px] h-[1px] bg-[#D0E1F3] my-4"></div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(["label", "age", "qnt"])
+const emit = defineEmits(["add", "subtract"])
+
+function incr() {
+    emit("add")
+}
+
+function decr() {
+    emit("subtract")
+}
+</script>
--- pages/tours/[id].vue
+++ pages/tours/[id].vue
@@ -1,76 +1,21 @@
 <template>
     <RoutePath :desc="selected[0].desc" />
     <div class="w-full justify-center items-center flex px-8">
-        <div class="w-[1384px] flex justify-start xl:flex-col gap-8">
+        <div class="w-[1384px] flex justify-start lg:flex-col gap-8">
             <SpecTourPart :selectedTour="selected[0]" />
-            <div class="w-[368px] p-6 rounded-lg h-[566px] border-[1px] border-solid border-[#D0E1F3]">
-                <div class="flex gap-2">
+            <div class="min-w-[310px] p-6 rounded-lg h-[566px] border-[1px] border-solid border-[#D0E1F3]">
+                <div class="flex gap-2 whitespace-pre-wrap">
                     <p class="font-light text-[#666C73] mr-2">Direction:</p>
                     <p class="text-[#000A15] font-medium">{{ selected[0].direction }}</p>
                 </div>
-                <div class="flex gap-2 mt-3 mb-6">
+                <div class="flex gap-2 mt-3 mb-6 sm:flex-col">
                     <p class="font-light text-[#666C73] mr-2">Start date and time:</p>
-                    <p class="text-[#000A15] font-medium">30 Jun, 2023 | 05:00</p>
+                    <p class="text-[#000A15] font-medium whitespace-normal">30 Jun, 2023 | 05:00</p>
                 </div>
-                <div>
-                    <div class="flex gap-2">
-                        <p class="text-[#19222C] mr-2">Adults</p>
-                        <p class="text-[#666C73] font-medium">(age 3-8)</p>
-                    </div>
-                    <div class="flex mt-1 bg-primaryBlue rounded-lg w-[160px]">
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="4" viewBox="0 0 16 4" fill="none">
-                                <path d="M16 0.474786V3.52685C16 3.75661 15.8139 3.94304 15.5838 3.94304H9.94226C9.71251 3.94304 6.0578 3.94304 6.0578 3.94304H0.416194C0.186254 3.94304 0 3.75661 0 3.52685V0.474786C0 0.244984 0.186254 0.0585938 0.416194 0.0585938H6.0578H9.94226H15.5838C15.8139 0.0585938 16 0.244984 16 0.474786Z" fill="white"/>
-                            </svg>
-                        </div>
-                        <div class="py-3 w-16 bg-white text-center border-[1px] border-solid border-[#E6E6E7] rounded-lg">0</div>
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg  py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                <path d="M16 6.47396V9.52602C16 9.75578 15.8138 9.94221 15.5838 9.94221H9.94224V15.5837C9.94224 15.8138 9.75581 15.9999 9.52605 15.9999H6.47398C6.24436 15.9999 6.05779 15.8138 6.05779 15.5837V9.94221H0.416193C0.186253 9.94221 0 9.75578 0 9.52602V6.47396C0 6.24416 0.186253 6.05777 0.416193 6.05777H6.05779V0.416192C6.05779 0.186161 6.24432 0 6.47398 0H9.52605C9.75581 0 9.94224 0.186161 9.94224 0.416192V6.05777H15.5838C15.8139 6.05777 16 6.24416 16 6.47396Z" fill="white"/>
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="max-w-[320px] h-[1px] bg-[#D0E1F3] my-4"></div>
-                </div>
-                <div>
-                    <div class="flex gap-2">
-                        <p class="text-[#19222C] mr-2">Child</p>
-                        <p class="text-[#666C73] font-medium">(age 3-8)</p>
-                    </div>
-                    <div class="flex mt-1 bg-primaryBlue rounded-lg w-[160px]">
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="4" viewBox="0 0 16 4" fill="none">
-                                <path d="M16 0.474786V3.52685C16 3.75661 15.8139 3.94304 15.5838 3.94304H9.94226C9.71251 3.94304 6.0578 3.94304 6.0578 3.94304H0.416194C0.186254 3.94304 0 3.75661 0 3.52685V0.474786C0 0.244984 0.186254 0.0585938 0.416194 0.0585938H6.0578H9.94226H15.5838C15.8139 0.0585938 16 0.244984 16 0.474786Z" fill="white"/>
-                            </svg>
-                        </div>
-                        <div class="py-3 w-16 bg-white text-center border-[1px] border-solid border-[#E6E6E7] rounded-lg">0</div>
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg  py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                <path d="M16 6.47396V9.52602C16 9.75578 15.8138 9.94221 15.5838 9.94221H9.94224V15.5837C9.94224 15.8138 9.75581 15.9999 9.52605 15.9999H6.47398C6.24436 15.9999 6.05779 15.8138 6.05779 15.5837V9.94221H0.416193C0.186253 9.94221 0 9.75578 0 9.52602V6.47396C0 6.24416 0.186253 6.05777 0.416193 6.05777H6.05779V0.416192C6.05779 0.186161 6.24432 0 6.47398 0H9.52605C9.75581 0 9.94224 0.186161 9.94224 0.416192V6.05777H15.5838C15.8139 6.05777 16 6.24416 16 6.47396Z" fill="white"/>
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="max-w-[320px] h-[1px] bg-[#D0E1F3] my-4"></div>
-                </div>
-                <div>
-                    <div class="flex gap-2">
-                        <p class="text-[#19222C] mr-2">Adults</p>
-                        <p class="text-[#666C73] font-medium">(age 3-8)</p>
-                    </div>
-                    <div class="flex mt-1 bg-primaryBlue rounded-lg w-[160px]">
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="4" viewBox="0 0 16 4" fill="none">
-                                <path d="M16 0.474786V3.52685C16 3.75661 15.8139 3.94304 15.5838 3.94304H9.94226C9.71251 3.94304 6.0578 3.94304 6.0578 3.94304H0.416194C0.186254 3.94304 0 3.75661 0 3.52685V0.474786C0 0.244984 0.186254 0.0585938 0.416194 0.0585938H6.0578H9.94226H15.5838C15.8139 0.0585938 16 0.244984 16 0.474786Z" fill="white"/>
-                            </svg>
-                        </div>
-                        <div class="py-3 w-16 bg-white text-center border-[1px] border-solid border-[#E6E6E7] rounded-lg">0</div>
-                        <div class="flex justify-center items-center bg-primaryBlue rounded-lg  py-3 w-14 text-white text-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                                <path d="M16 6.47396V9.52602C16 9.75578 15.8138 9.94221 15.5838 9.94221H9.94224V15.5837C9.94224 15.8138 9.75581 15.9999 9.52605 15.9999H6.47398C6.24436 15.9999 6.05779 15.8138 6.05779 15.5837V9.94221H0.416193C0.186253 9.94221 0 9.75578 0 9.52602V6.47396C0 6.24416 0.186253 6.05777 0.416193 6.05777H6.05779V0.416192C6.05779 0.186161 6.24432 0 6.47398 0H9.52605C9.75581 0 9.94224 0.186161 9.94224 0.416192V6.05777H15.5838C15.8139 6.05777 16 6.24416 16 6.47396Z" fill="white"/>
-                            </svg>
-                        </div>
-                    </div>
-                    <div class="max-w-[320px] h-[1px] bg-[#D0E1F3] my-4"></div>
+                <div class="flex flex-col gap-2 lg:flex-row lg:justify-between lg:flex-wrap">
+                    <QntInput @add="addQnt(adults)" @subtract="subtrQnt(adults)" :qnt="adults.qnt" :label="'Adults'" :age="'3-8'" />
+                    <QntInput @add="addQnt(children)" @subtract="subtrQnt(children)" :qnt="children.qnt" :label="'Child'" :age="'3-8'" />
+                    <QntInput @add="addQnt(infants)" @subtract="subtrQnt(infants)" :qnt="infants.qnt" :label="'Infant'" :age="'0-3'" />
                 </div>
                 <div class="flex justify-between">
                     <p class="text-xl text-[#666C73] font-medium">Total Price:</p>
@@ -94,6 +39,16 @@ useHead({
     title: `Tour: ${route.params.id}`
 })
 
+let adults = reactive({
+    qnt: 0
+})
+const children = reactive({
+    qnt: 0
+})
+const infants = reactive({
+    qnt: 0 
+})
+
 import { useToursStore } from "../../stores/store.js"
 const { tours } = useToursStore();
 
@@ -101,4 +56,14 @@ console.log(tours);
 
 const selected = tours.filter(tour => tour.id == route.params.id);
 console.log(selected[0]);
+
+function addQnt(param) {
+    param.qnt++
+}
+
+function subtrQnt(param) {
+    if (param.qnt > 0) {
+        param.qnt--
+    }
+}
 </script>
