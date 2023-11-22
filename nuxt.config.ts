16ac06bd807e24f447719caa4a6a6f89b9d3b25e
mariakhachatryann <mariakhachatryan23@gmail.com>
2023-11-22 20:44:12 +0400

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
             "Noto Sans Armenian": [100, 200, 300, 400, 500, 600, 700, 800, 900],
             "Rubik": [400]
         },
-        display: 'fallback',
+        display: 'block',
         download: true
     },
     image: {
